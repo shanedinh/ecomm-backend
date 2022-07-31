69f669fda36dbb34d87d744b90b44c9a3491cc99
Shane Dinh <shane.dinh5@gmail.com>
2022-07-31 02:12:27 -0500

    product model and api get/post created.

---
+++ models/Category.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+
+class Category extends Model {}
+
+// Category.init(
+//   {
+//     // define columns
+//     id: {
+//       // type of data
+//       type: DataTypes.INTEGER,
+//       // NOT NULL
+//       autoNull: false,
+//       // instruct this is primary key
+//       primaryKey: true,
+//       // auto increment on
+//       autoIncrement: true,
+//     },
+//     product_name: {
+//       type: DataTypes.STRING,
+//       autoNull: false,
+//       validate: {
+//         isProdName: true,
+//       },
+//     },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: "category",
+//   }
+// );
+
+module.exports = Category;
---
+++ models/Product.js
@@ -0,0 +1,58 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require("sequelize");
+// import our database connection from config.js
+const sequelize = require("../config/connection");
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    // define columns
+    id: {
+      // data type
+      type: DataTypes.INTEGER,
+      // NOT NULL
+      allowNull: false,
+      // instruct primary key
+      primaryKey: true,
+      // auto increment on
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isProdName: true,
+      },
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isPrice: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "product",
+  }
+);
+
+module.exports = Product;
