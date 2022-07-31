501517f6ed50298eb6308da0827888d2a153ceaa
Shane Dinh <shane.dinh5@gmail.com>
2022-07-31 12:27:36 -0500

    foreign keys updated in model/index.js

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "product_id",
+  foreignKey: "category_id",
 });
 // Categories have many Products
 Category.hasMany(Product, {
@@ -16,11 +16,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
+  foreignKey: "tag_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
+  foreignKey: "tag_id",
 });
 
 module.exports = {
